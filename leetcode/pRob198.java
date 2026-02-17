30f516d60c8739ab6784c4655ae5b2039afca97a
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2026-02-17 20:15:14 +0530

    added leetcode qs 2068 optimal solution

---
+++ leetcode/pRob198.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class pRob198 {
+    static void main() {
+        int n = -10;
+        String binary = Integer.toString(n,2);
+        System.out.println(binary);
+    }
+}
