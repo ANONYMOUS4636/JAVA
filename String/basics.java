a71d2e84ee1f35e6deb21cb5ca2a123c761c6925
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2026-05-17 11:40:02 +0530

    anonymous

--- String/basics.java
+++ String/basics.java
@@ -2,7 +2,14 @@ package String;
 
 public class basics {
     static void main() {
-        String str="";
-        System.out.println(str);
+        int l=24;
+        int r=40;
+        int k=9;
+        int left=(int)Math.ceil(Math.pow(l,1.0/k));
+        int right=(int)Math.round(Math.pow(r,1.0/k));
+        System.out.println(left);
+        System.out.println(right);
+        System.out.println((right-left)+1);
     }
-}
+    }
+
